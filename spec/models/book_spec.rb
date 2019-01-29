2eb1171b048f90d238a5a04bfd976b61c912074c
plapicola <p.lapicola@gmail.com>
2019-01-29 16:30:28 -0700

    Adds database schema to README and initial model and tests for book model
    Co-authored-by: Julia Marco <marcootj@gmail.com>

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Book do
+  describe 'validations' do
+  end
+
+  describe 'relationhsips' do
+  end
+
+  describe 'class methods' do
+  end
+
+  describe 'instance methods' do
+  end
+end
