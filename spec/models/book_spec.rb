b4e4b941c018962aca1e2766b221cbac04f53755
plapicola <p.lapicola@gmail.com>
2019-01-29 16:39:48 -0700

    Adds book model spec file
    Co-authored-by: Julia Marco <marcootj@gmail.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Book do
   describe 'validations' do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :year}
+    it {should validate_presence_of :page_count}
   end
 
-  describe 'relationhsips' do
+  describe 'relationships' do
+    it {should have_many :authors}
+    xit {should have_many :reviews}
   end
 
   describe 'class methods' do
