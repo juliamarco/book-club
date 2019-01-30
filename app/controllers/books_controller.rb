1c2d8f848e53a554dbe911f7ed3cac43d6933bd1
plapicola <p.lapicola@gmail.com>
2019-01-30 16:58:13 -0700

    Adds new page for books, linking to new author page for creation of authors not currently present on the site
    Co-authored-by: Julia Marco <marcootj@gmail.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,24 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+    @authors = Author.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :page_count, :year, :cover_image, author_ids: [])
+  end
+
 end
