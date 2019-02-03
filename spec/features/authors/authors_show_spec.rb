0c2dad318234a4223022976d2d5bfca034825f5d
plapicola <p.lapicola@gmail.com>
2019-02-03 09:26:59 -0700

    Adds tests for top review display on author show page

--- spec/features/authors/authors_show_spec.rb
+++ spec/features/authors/authors_show_spec.rb
@@ -2,24 +2,45 @@ require 'rails_helper'
 
 describe 'When I visit /authors/:id' do
   context 'as a visitor' do
+    before :each do
+      @stephen_king = Author.create(name: "Stephen King")
+      @herman_melville = Author.create(name: "Herman Melville")
+      @book_1 = Book.create(title: "IT", page_count: 1168, year: 1986, authors: [@stephen_king, @herman_melville], cover_image: "https://prodimage.images-bn.com/pimages/9781501142970_p0_v3_s550x406.jpg")
+      @book_2 = Book.create(title: "The Shining", page_count: 688, year: 1977, authors: [@stephen_king], cover_image: "https://prodimage.images-bn.com/pimages/9780345806789_p0_v1_s550x406.jpg")
+      @tim = User.create(name: "tim")
+      @review = @book_1.reviews.create(user: @tim, title: "So spooky", rating: 4, review_text: "Literally made me cry.")
+    end
+
     it 'displays all book titles by that author' do
 
-      stephen_king = Author.create(name: "Stephen King")
-      herman_melville = Author.create(name: "Herman Melville")
-      book_1 = Book.create(title: "IT", page_count: 1168, year: 1986, authors: [stephen_king, herman_melville], cover_image: "https://prodimage.images-bn.com/pimages/9781501142970_p0_v3_s550x406.jpg")
-      book_2 = Book.create(title: "The Shining", page_count: 688, year: 1977, authors: [stephen_king], cover_image: "https://prodimage.images-bn.com/pimages/9780345806789_p0_v1_s550x406.jpg")
 
-      visit author_path(herman_melville)
+      visit author_path(@herman_melville)
 
-      expect(page).to have_content(book_1.title)
-      expect(page).to have_content("#{stephen_king.name}")
-      expect(page).to have_content(book_1.page_count)
-      expect(page).to have_content(book_1.year)
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content("#{@stephen_king.name}")
+      expect(page).to have_content(@book_1.page_count)
+      expect(page).to have_content(@book_1.year)
       expect(page).to have_xpath('//img[@src="https://prodimage.images-bn.com/pimages/9781501142970_p0_v3_s550x406.jpg"]')
+      expect(page).to have_content("Book has no reviews")
       expect(page).to_not have_xpath('//img[@src="https://prodimage.images-bn.com/pimages/9780345806789_p0_v1_s550x406.jpg"]')
-      expect(page).to_not have_content(book_2.year)
-      expect(page).to_not have_content(book_2.page_count)
-    end
+      expect(page).to_not have_content(@book_2.year)
+      expect(page).to_not have_content(@book_2.page_count)
     end
 
+    it 'can display multiple book titles for author' do
+
+      visit author_path(@stephen_king)
+
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content("#{@herman_melville.name}")
+      expect(page).to have_content("Page Count: #{@book_1.page_count}")
+      expect(page).to have_content("Year: #{@book_1.year}")
+      expect(page).to have_xpath('//img[@src="https://prodimage.images-bn.com/pimages/9781501142970_p0_v3_s550x406.jpg"]')
+      expect(page).to have_content("Top Review:\n#{@review.title}\n#{@review.user}\n#{@review.review_text}")
+      expect(page).to have_xpath('//img[@src="https://prodimage.images-bn.com/pimages/9780345806789_p0_v1_s550x406.jpg"]')
+      expect(page).to have_content("Year: #{@book_2.year}")
+      expect(page).to have_content("Page Count: #{@book_2.page_count}")
+      expect(page).to have_content("Book has no reviews")
+    end
   end
+end
