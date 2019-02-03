b1d20054dc38912a57227263250160fa12319219
plapicola <p.lapicola@gmail.com>
2019-02-02 17:42:10 -0700

    Adds top_reviewers method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: {minimum: 1}
 
   has_many :reviews
+
+  def self.top_reviewers(count)
+    select("users.*, COUNT(reviews.id) as num_reviews")
+    .joins(:reviews)
+    .group(:id)
+    .order("num_reviews DESC")
+    .order(id: :asc)
+    .limit(count)
+  end
 end
