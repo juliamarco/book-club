ae05a08137cee2f9362b85f70b28c85e1cec2c77
Julia Marco <marcootj@gmail.com>
2019-02-03 17:29:24 -0700

    It finally works.
    Co-authored-by:  Peter Lapicola <41562392+plapicola@users.noreply.github.com>

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,11 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  def destroy
+    author = Author.find(params[:id])
+    author.books.each {|b| b.destroy}
+    author.destroy
+    redirect_to books_path
+  end
+
 end
