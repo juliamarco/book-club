0340245595ed75dc3616d58b6a38265e1be7ffbf
Julia Marco <marcootj@gmail.com>
2019-02-05 16:22:41 -0700

    Adds final styling changes
    Co-authored-by:  Peter Lapicola <41562392+plapicola@users.noreply.github.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -5,7 +5,7 @@ describe 'when I visit /' do
     it 'displays a navigation bar' do
       visit root_path
 
-      expect(page).to have_content("Welcome!")
+      expect(page).to have_content("Welcome to our Book Club")
 
       click_link("All Books")
       expect(current_path).to eq(books_path)
